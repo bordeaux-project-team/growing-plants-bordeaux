9f524a1db972c82d74e9443828bc765e7f1d294e
Nhan Nguyen <nhannn@imt-soft.com>
2020-09-30 10:55:46 +0700

    [BE] Update log for environment

--- back-end/GrowingPlants/GrowingPlants.BusinessLogic/Services/PlantingEnvironmentService.cs
+++ back-end/GrowingPlants/GrowingPlants.BusinessLogic/Services/PlantingEnvironmentService.cs
@@ -26,7 +26,7 @@ namespace GrowingPlants.BusinessLogic.Services
         public async Task<ApiResult<List<PlantingEnvironment>>> GetPlantingEnvironmentsByUser(int userId)
         {
             var environments = await _unitOfWork.PlantingEnvironmentRepository.GetByUserId(userId);
-            _logger.LogInformation(JsonConvert.SerializeObject(environments));
+            _logger.LogInformation(environments.Count);
             return new ApiResult<List<PlantingEnvironment>>
             {
                 ApiCode = ApiCode.Success,
